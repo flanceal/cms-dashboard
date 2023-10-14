e1c7ffa3c9d1fc0b7e545a53faf7511d6e7692f3
danil <126198486+flanceal@users.noreply.github.com>
2023-10-14 22:44:56 +0200

    Added models for Customer, Product and Order

--- backend/system_cms/models.py
+++ backend/system_cms/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 from .managers import CustomUserManager
 
@@ -18,4 +19,43 @@ class CustomUserModel(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+
+class CustomerModel(models.Model):
+    full_name = models.CharField(max_length=110, null=False, blank=False)
+    email = models.EmailField(unique=True, null=False, blank=False)
+    phone_number = PhoneNumberField(unique=True, blank=True)
+    address = models.CharField(blank=True, max_length=300)
+    is_deleted = models.BooleanField(blank=True, default=False)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=175, blank=False)
+    description = models.TextField(max_length=500, null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=False, blank=False)
+    stock = models.PositiveIntegerField(default=0, null=True, blank=False)
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(CustomerModel, on_delete=models.DO_NOTHING)
+    products = models.ManyToManyField(Product, through='OrderItem')
+    total_price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
+    order_date = models.DateTimeField(auto_now_add=True, null=False, blank=True)
+
+    def __str__(self):
+        return f"Order #{self.id}"
+
+    def calculate_total_price(self):
+        total = sum([item.quantity * item.product.price for item in self.orderitem_set.all()])
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.PositiveIntegerField(default=1)
 
